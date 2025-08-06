648b35d63cfd3c7122eb64785a4c06e504e72498
Elvis Kerebi <kerebipreye@gmail.com>
2025-08-07 01:26:39 +0200

    initial project setup

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Public_Sans } from "next/font/google";
+import "./globals.css";
+
+const publicSans = Public_Sans({
+  variable: "--font-public-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Fintrack - your finance partner",
+  description: "Fintra is a platform for managing your finances.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${publicSans.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <section>
+      <h1>Hello World</h1>
+    </section>
+  );
+}
